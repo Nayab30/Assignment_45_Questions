1169aef07371433a8040f4c333fc7301cd4484ed
[nayab aslam] <[aslamnayab56@gmail.com]>
2024-03-03 11:56:32 +0500

    first commit

--- main.js
+++ main.js
@@ -1,56 +1,56 @@
 //Assignment 1
 console.log("Hello World");
-//Question 2
+//Question 2 (Personal message)
 var pname = "Elric";
 console.log("\"Hello ".concat(pname, ",would you like to some python today?\""));
-//Question 3
+//Question 3 (Name cases)
 console.log("Lowrecase:", pname.toLowerCase());
 console.log("Uppercase:", pname.toUpperCase());
 console.log("Title case:", pname.replace("Elric", "Elric"));
-//Question 4
+//Question 4 (Famous Quote)
 console.log("Albert Einstein once said, \u201CA person who never made a mistake never tried anything new.\u201D");
-//Question 5
+//Question 5 (famous Quote 2)
 var famous_person = "Albert Einstein";
 console.log("".concat(famous_person, ",once said \u201CA person who never made a mistake never tried anything new.\u201D "));
-//Question 6
+//Question 6 (Stripping Name)
 var personName = "\n\t Nayab Aslam \t\n";
 console.log(personName);
 var stripped = personName.trim();
 console.log(stripped);
-//Question 7&8
+//Question 7&8 (Number Eight)
 console.log(5 + 3);
 console.log(11 - 3);
 console.log(4 * 2);
 console.log(16 / 2);
-//Question 9
+//Question 9 (Favorite Number)
 var fNumber = 99;
 console.log("My favourite number is ".concat(fNumber));
-//Question 10
+//Question 10 (Adding Comments)
 //My anme is Nayab Aslam
 //Date:23/2/24
 // This program rum hello
 console.log("Hello");
-//Question 11
+//Question 11 (Names store in array)
 var member = ['Sana', 'Sara', 'Zoya', 'Nida'];
 for (var i = 0; i < member.length; i++) {
     console.log(member[i]);
 }
-//Question 12
+//Question 12 (Greetings)
 var message = 'How are you:\t';
 for (var i = 0; i < member.length; i++) {
     console.log(message + member[i]);
 }
-//Question 13
+//Question 13 (Your own array)
 var transportation = ["Car", "Bike", "Bus"];
 for (var i = 0; i < transportation.length; i++) {
     console.log("I would like to own a: " + transportation[i]);
 }
-//Question 14
+//Question 14( Guest list)
 var guestName = ["Nida", "Sara", "Sana"];
 for (var i = 0; i < guestName.length; i++) {
     console.log("Venerable Madam: " + guestName[i] + ",\n I invited you in a dinner on tomorrow\n\n Thank You");
 }
-//Question 15
+//Question 15 (Changing guest list)
 var old_guest = "Sara";
 var new_guest = "Mariyam";
 guestName[1] = new_guest;
--- main.ts
+++ main.ts
@@ -1,60 +1,60 @@
 //Assignment 1
 console.log("Hello World")
-//Question 2
+//Question 2 (Personal message)
 let pname:string="Elric";
 console.log(`"Hello ${pname},would you like to some python today?"`)
-//Question 3
+//Question 3 (Name cases)
 console.log("Lowrecase:",pname.toLowerCase())
 console.log("Uppercase:",pname.toUpperCase())
 console.log("Title case:",pname.replace("Elric","Elric"))
-//Question 4
+//Question 4 (Famous Quote)
 console.log(`Albert Einstein once said, “A person who never made a mistake never tried anything new.”`)
-//Question 5
+//Question 5 (famous Quote 2)
 let famous_person:string="Albert Einstein";
 console.log(`${famous_person},once said “A person who never made a mistake never tried anything new.” `)
-//Question 6
+//Question 6 (Stripping Name)
 let personName:string = "\n\t Nayab Aslam \t\n";
 console.log(personName);
 let stripped = personName.trim();
 console.log(stripped);
-//Question 7&8
+//Question 7&8 (Number Eight)
 console.log(5+3);
 console.log(11-3);
 console.log(4*2);
 console.log(16/2);
-//Question 9
+//Question 9 (Favorite Number)
 let fNumber: Number=99;
 console.log(`My favourite number is ${fNumber}`);
-//Question 10
+//Question 10 (Adding Comments)
 //My anme is Nayab Aslam
 //Date:23/2/24
 // This program rum hello
 console.log("Hello");
-//Question 11
+//Question 11 (Names store in array)
 let member:string[]= ['Sana','Sara','Zoya','Nida'];
 for(let i=0; i<member.length; i++)
 {
     console.log(member[i]);
 }
-//Question 12
+//Question 12 (Greetings)
 let message:string='How are you:\t';
 for(let i=0; i<member.length; i++)
 {
     console.log(message + member[i]);
 }
-//Question 13
+//Question 13 (Your own array)
 let transportation:string[]=["Car","Bike","Bus"];
 for(let i=0; i<transportation.length; i++)
 {
     console.log("I would like to own a: " + transportation[i]);
 }
-//Question 14
+//Question 14( Guest list)
 let guestName:string[]=["Nida","Sara","Sana"]
 for(let i=0; i<guestName.length; i++)
 {
     console.log("Venerable Madam: " + guestName[i] +",\n I invited you in a dinner on tomorrow\n\n Thank You")
 }
-//Question 15
+//Question 15 (Changing guest list)
 let old_guest:string="Sara";
 let new_guest:string="Mariyam"
 guestName[1] = new_guest;
